f6cb25202ca5b4b3b763ab0cac23410467bee7ec
Koshys <koshys.osu@gmail.com>
2023-06-16 14:04:50 +0300

    added basic layout for page, added assets folder with profile picture and folder for js scripts with some animation to use

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function() {
+    // Select the elements to animate
+    var elements = document.querySelectorAll('.fade-in');
+  
+    // Apply the animation to each element
+    elements.forEach(function(element) {
+      element.style.opacity = 0;
+      element.style.transition = 'opacity 1s ease-in-out';
+  
+      // Trigger the animation when the element is in the viewport
+      var observer = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting) {
+          element.style.opacity = 1;
+          observer.unobserve(element);
+        }
+      }, { threshold: 0.5 });
+  
+      observer.observe(element);
+    });
+  });
+  
