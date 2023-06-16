c320e67544f01ff9ed618c85b182074b54570d5b
Koshys <koshys.osu@gmail.com>
2023-06-16 16:56:17 +0300

    adding all content to page that i wanted

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', function() {
     // Select the elements to animate
     var elements = document.querySelectorAll('.fade-in');
-  
+
     // Apply the animation to each element
     elements.forEach(function(element) {
       element.style.opacity = 0;
       element.style.transition = 'opacity 1s ease-in-out';
-  
+
       // Trigger the animation when the element is in the viewport
       var observer = new IntersectionObserver(function(entries) {
         if (entries[0].isIntersecting) {
@@ -14,8 +14,7 @@ window.addEventListener('load', function() {
           observer.unobserve(element);
         }
       }, { threshold: 0.5 });
-  
+
       observer.observe(element);
     });
   });
-  
